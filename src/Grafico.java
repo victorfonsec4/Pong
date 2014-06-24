ea00b2f5396957c2059e421cc92b993a796c3f95
Victor <victor@victor-RF511-RF411-RF711.(none)>
2014-06-23 22:24:49 -0300

    gerando engine grafico

--- src/Grafico.java
+++ src/Grafico.java
@@ -3,28 +3,44 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class Grafico extends JFrame
 {
-	Image fundoPreto;
+	List<Imagem> imagensDesenhar;
 	public Grafico()
 	{
 		super("Pong");
+
+		imagensDesenhar = new ArrayList<Imagem>();
+		Imagem fundoPreto;
+
 		try
 		{
-			fundoPreto = ImageIO.read(new File("imagens/black.jpg"));
+			fundoPreto = new Imagem(0, 0, ImageIO.read(new File("imagens/black.jpg")));
+			imagensDesenhar.add(fundoPreto);
 		} 
 		catch(IOException e)
 		{
 			System.out.println("failed to read imagens/black.jpg");
 		}
+
+	}
+
+	public void desenhar(Imagem img)
+	{
+		imagensDesenhar.add(img);
 	}
 
 	public void paint(Graphics g)
 	{
-		g.drawImage(fundoPreto, 0, 0, null);
+		for(Imagem img : imagensDesenhar)
+		{
+			g.drawImage(img.imagem, img.X, img.Y, null);
+		}
 	}
 }
---
+++ src/Imagem.java
@@ -0,0 +1,16 @@
+import java.awt.Image;
+
+
+public class Imagem 
+{
+	public int X;
+	public int Y;
+	public Image imagem;
+	
+	public Imagem(int X, int Y, Image imagem)
+	{
+		this.X = X;
+		this.Y = Y;
+		this.imagem = imagem;
+	}
+}
