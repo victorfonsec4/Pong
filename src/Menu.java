1fc7a403045bcdc3a32406282544882b6b21ef09
Cowalters <cesarpereiradf@hotmail.com>
2014-06-27 00:48:52 -0300

    cesar sugao

---
+++ src/Menu.java
@@ -0,0 +1,6 @@
+import javax.swing.JPanel;
+
+public class Menu extends JPanel
+{
+
+}
---
+++ src/PanelSP.java
@@ -0,0 +1,69 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+
+public class PanelSP extends JPanel
+{
+	List<Imagem> imagensDesenhar;
+	public boolean executar = false;
+	private int pontJogador1, pontJogador2;
+	Font fonte;
+
+	public PanelSP()
+	{
+		pontJogador1 = 0;
+		pontJogador2 = 0;
+
+		fonte = new Font("Score", Font.PLAIN, 300);
+
+		this.setBackground(Color.BLACK);
+		imagensDesenhar = new ArrayList<Imagem>();
+	}
+
+	public void score(int p1, int p2)
+	{
+		pontJogador1 = p1;
+		pontJogador2 = p2;
+	}
+
+	public void desenhar(Imagem img)
+	{
+		imagensDesenhar.add(img);
+	}
+	
+	public void boost(int posic) throws IOException
+	{
+		Imagem img = new Imagem(50,posic,ImageIO.read(new File("imagens/doge.png")));
+		imagensDesenhar.add(img);
+	}
+
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+
+		g.setFont(fonte);
+		g.setColor(Color.WHITE);
+		g.drawString("" + pontJogador1, this.size().width/2 - this.size().width/5, this.size().height*1/3);
+		g.drawString("" + pontJogador2, this.size().width/2, this.size().height*1/3);
+
+		if(executar)
+		{
+			for(int i = 0; i < imagensDesenhar.size(); i++)
+			{
+				Imagem img = imagensDesenhar.get(i);
+				g.drawImage(img.imagem, img.X, img.Y, this);
+			}
+			imagensDesenhar.clear();
+			executar = false;
+		}
+	}
+}
